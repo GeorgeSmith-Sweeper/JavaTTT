53b40f918ba26713d5bdc85c78a65f3aa4e7f756
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-12-27 12:10:24 -0500

    simplify board tests: create setUp with @BeforeEach, remove duplicated logic

--- src/test/java/TestBoard.java
+++ src/test/java/TestBoard.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,101 +7,75 @@ import java.util.Arrays;
 
 class TestBoard {
 
-    @Test
-    void createBoard() {
-        int size = 3;
-        Board board = new Board(size);
-        ArrayList<Integer> expectedBoard = new ArrayList<Integer>(size * size);
-        for (int i = 0; i < size*size; i++) {
-            expectedBoard.add(i);
-        }
+    private Board testBoard;
+    private int size;
 
-        assertEquals(board.createBoard(), expectedBoard);
+    @BeforeEach
+    public void setUp() {
+        size = 3;
+        testBoard = new Board(size);
+        testBoard.createBoard();
     }
 
     @Test
-    void getSpacesReturnsTheCurrentStateOfTheBoard() {
-        int size = 3;
-        Board board = new Board(size);
+    void createBoardMakesAnArrayFilledWithInts() {
         ArrayList<Integer> expectedBoard = new ArrayList<Integer>(size * size);
         for (int i = 0; i < size*size; i++) {
             expectedBoard.add(i);
         }
-        board.createBoard();
 
-        assertEquals(expectedBoard, board.getSpaces());
+        assertEquals(expectedBoard, testBoard.getSpaces());
     }
 
     @Test
     void spaceWithinBoundsReturnsTrueIfSpaceIsWithinBounds() {
         String userInput = "0";
-        int size = 3;
-        Board board = new Board(size);
-        board.createBoard();
 
-        assertTrue(board.spaceWithinBounds(userInput));
+        assertTrue(testBoard.spaceWithinBounds(userInput));
     }
 
     @Test
     void spaceWithinBoundsReturnsFalseIfSpaceIsNotWithinBounds() {
         String userInput = "13";
-        int size = 3;
-        Board board = new Board(size);
-        board.createBoard();
 
-        assertFalse(board.spaceWithinBounds(userInput));
+        assertFalse(testBoard.spaceWithinBounds(userInput));
     }
 
     @Test
     void spaceWithinBoundsReturnsFalseIfInputIsNotANumber() {
         String userInput = "S";
-        int size = 3;
-        Board board = new Board(size);
-        board.createBoard();
 
-        assertFalse(board.spaceWithinBounds(userInput));
+        assertFalse(testBoard.spaceWithinBounds(userInput));
     }
 
     @Test
     void spaceWithinBoundsReturnsFalseIfInputIsEmpty() {
         String userInput = "";
-        int size = 3;
-        Board board = new Board(size);
-        board.createBoard();
 
-        assertFalse(board.spaceWithinBounds(userInput));
+        assertFalse(testBoard.spaceWithinBounds(userInput));
     }
 
     @Test
     void updateSpace() {
         String userInput = "1";
         String userSymbol = "X";
-        int size = 3;
-        Board board = new Board(size);
-        board.createBoard();
         ArrayList expectedBoard = new ArrayList(Arrays.asList(0, "X", 2, 3, 4, 5, 6, 7, 8));
         MockPlayer playerOne = new MockPlayer("X");
-        board.updateSpace(userInput, playerOne);
-        assertEquals(expectedBoard, board.getSpaces());
+        testBoard.updateSpace(userInput, playerOne);
+        assertEquals(expectedBoard, testBoard.getSpaces());
     }
 
     @Test
     void gameIsTieReturnsTrueIfBoardIsFull() {
-        int size = 3;
-        Board board = new Board(size);
-        board.createBoard();
         ArrayList expectedBoard = new ArrayList(Arrays.asList("X", "X", "X", "X", "X", "X", "X", "X", "X"));
 
-        assertTrue(board.gameIsTie(expectedBoard));
+        assertTrue(testBoard.gameIsTie(expectedBoard));
     }
 
     @Test
     void gameIsTieReturnsFalseIfBoardIsNotFull() {
-        int size = 3;
-        Board board = new Board(size);
-        board.createBoard();
         ArrayList expectedBoard = new ArrayList(Arrays.asList(0, "X", "X", "X", "X", "X", "X", "X", "X"));
 
-        assertFalse(board.gameIsTie(expectedBoard));
+        assertFalse(testBoard.gameIsTie(expectedBoard));
     }
 }
