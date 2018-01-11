d917d0dbf23ae1ceca15a15dad2fb44d88164142
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-01-11 16:34:34 -0500

    users can select tokens for players

--- src/main/java/com/EighthLight/app/Config.java
+++ src/main/java/com/EighthLight/app/Config.java
@@ -1,34 +1,61 @@
 package com.EighthLight.app;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Config implements IConfig{
 
     private IUserInterface ui;
     private ArrayList<IPlayer> players = new ArrayList();
+    private ArrayList<String> symbols = new ArrayList<>();
 
     public Config(IUserInterface ui) {
         this.ui = ui;
+        setUpGame();
     }
 
-    public ArrayList<IPlayer> getPlayers() {
+    private void setUpGame() {
+        setSymbols();
+        setPlayers();
+    }
 
+    private void setPlayers() {
         ui.display(Constants.GAME_MODE_PROMPT);
         String userInput = ui.getInput();
 
         if (userInput.equals("1")) {
-            Player playerOne = new Player("X", ui);
-            Player playerTwo = new Player("O", ui);
+            Player playerOne = new Player(symbols.get(0), ui);
+            Player playerTwo = new Player(symbols.get(1), ui);
             players.add(playerOne);
             players.add(playerTwo);
         }
 
         if (userInput.equals("2")) {
-            Player playerOne = new Player("X", ui);
-            Ai playerTwo = new Ai("O");
+            Player playerOne = new Player(symbols.get(0), ui);
+            Ai playerTwo = new Ai(symbols.get(1));
             players.add(playerOne);
             players.add(playerTwo);
         }
+    }
+
+    private void setSymbols() {
+        ArrayList<String> prompts = new ArrayList(Arrays.asList("Pick a symbol for player 1", "Pick a symbol for player 2"));
+        for (String prompt : prompts) {
+            ui.display(prompt);
+            String symbol = ui.getInput();
+            while (symbols.contains(symbol)) {
+                ui.display("Symbol already selected, please pick a different symbol.");
+                symbol = ui.getInput();
+            }
+            symbols.add(symbol);
+        }
+    }
+
+    public ArrayList<IPlayer> getPlayers() {
         return players;
+    }
+
+    public ArrayList getSymbols() {
+        return symbols;
     }
 }
--- src/test/java/com/EighthLight/app/TestConfig.java
+++ src/test/java/com/EighthLight/app/TestConfig.java
@@ -14,7 +14,7 @@ public class TestConfig {
     @Test
     void gameMode1StartsWithTwoHumans() {
 
-        MockUi ui = new MockUi(new ArrayList(Arrays.asList("1")));
+        MockUi ui = new MockUi(new ArrayList(Arrays.asList("O", "X", "1")));
         Config config = new Config(ui);
 
         ArrayList players = config.getPlayers();
@@ -25,7 +25,7 @@ public class TestConfig {
     @Test
     void gameMode2StartsWithAHumanAndAi() {
 
-        MockUi ui = new MockUi(new ArrayList(Arrays.asList("2")));
+        MockUi ui = new MockUi(new ArrayList(Arrays.asList("O", "X", "2")));
         Config config = new Config(ui);
 
         ArrayList players = config.getPlayers();
@@ -35,15 +35,41 @@ public class TestConfig {
 
     @Test
     void usersArePromptedOnceToSelectAGameModeWithCorrectInput() {
-        MockUi ui = new MockUi(new ArrayList(Arrays.asList("1")));
+        MockUi ui = new MockUi(new ArrayList(Arrays.asList("O", "X", "1")));
         Config config = new Config(ui);
         ArrayList players = config.getPlayers();
 
-        assertEquals(Constants.GAME_MODE_PROMPT, ui.getDisplayArgs().get(0));
-        assertEquals(1, ui.getNumTimesGetInputCalled());
+        assertEquals("Pick a symbol for player 1", ui.getDisplayArgs().get(0));
+        assertEquals("Pick a symbol for player 2", ui.getDisplayArgs().get(1));
+        assertEquals(Constants.GAME_MODE_PROMPT, ui.getDisplayArgs().get(2));
+        assertEquals(3, ui.getNumTimesGetInputCalled());
+    }
+
+    @Test
+    void usersCanSelectAnyTokenWhenSettingUpTheGameWithCorrectInput() {
+        MockUi ui = new MockUi(new ArrayList(Arrays.asList("X", "O", "1")));
+        Config config = new Config(ui);
+        ArrayList symbols = config.getSymbols();
+
+        assertEquals("Pick a symbol for player 1", ui.getDisplayArgs().get(0));
+        assertEquals("Pick a symbol for player 2", ui.getDisplayArgs().get(1));
+        assertEquals(3, ui.getNumTimesGetInputCalled());
+        assertEquals("X", symbols.get(0));
+        assertEquals("O", symbols.get(1));
+    }
+
+    @Test
+    void usersCantSelectSameTokens() {
+        MockUi ui = new MockUi(new ArrayList(Arrays.asList("X", "X", "O", "1")));
+        Config config = new Config(ui);
+        ArrayList symbols = config.getSymbols();
+
+        assertEquals("Pick a symbol for player 1", ui.getDisplayArgs().get(0));
+        assertEquals("Pick a symbol for player 2", ui.getDisplayArgs().get(1));
+        assertEquals("Symbol already selected, please pick a different symbol.", ui.getDisplayArgs().get(2));
+
+        // ui.display is called
+        // ui.getInput is called
+        // ui.getDisplayArgs = token prompt msg
     }
 }
-
-// inputs: String gameMode selected
-// output: ArrayList of player instances (HVH = Player, Player)
-// HVC = Player, Ai
