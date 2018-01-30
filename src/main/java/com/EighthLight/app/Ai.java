cf386afe09340d45d4d496df8db953d816c51617
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-01-30 13:56:54 -0500

    remove all difficulty logic from ai

--- src/main/java/com/EighthLight/app/Ai.java
+++ src/main/java/com/EighthLight/app/Ai.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class Ai implements IPlayer{
     private String aiSymbol;
     private String humanSymbol;
-//    private String difficulty;
     private IStrategy difficulty;
     private IBoard ourBoard;
 
@@ -18,90 +17,6 @@ public class Ai implements IPlayer{
 
     public String getSymbol() {
         return this.aiSymbol;
-    }
-
-    public int findBestMove(Map<Integer, Integer>scoredSpaces)  {
-        int bestSpace = 0;
-        int highestScore = -1000;
-
-        for (Map.Entry<Integer, Integer> entry : scoredSpaces.entrySet()) {
-            int space = entry.getKey();
-            int score = entry.getValue();
-            if (score >= highestScore) {
-                highestScore = score;
-                bestSpace = space;
-            }
-        }
-        return bestSpace;
-    }
-
-    public int heuristicValue(ArrayList boardState, int depth) {
-        if (ourBoard.hasAPlayerWon(boardState, aiSymbol)) {
-            return 1000 / depth;
-        } else if (ourBoard.hasAPlayerWon(boardState, humanSymbol)) {
-            return -1000 / depth;
-        } else {
-            return 0;
-        }
-    }
-
-    private int miniMax(ArrayList boardState, int depth, int pointOfView) {
-        String playerWhoMovesNow = pointOfView == 1 ? humanSymbol : aiSymbol;
-        ArrayList newBoard = new ArrayList();
-        newBoard.addAll(boardState);
-        ArrayList<Integer> emptySpaces = ourBoard.findEmptySpaces(boardState);
-        if (gameIsOver(boardState) || depth == 0) {
-            return pointOfView * heuristicValue(boardState, depth);
-        }
-
-        int bestValue = 1000;
-
-        for (int space : emptySpaces) {
-            newBoard.set(space, playerWhoMovesNow);
-            int value = -miniMax(newBoard, depth - 1, -pointOfView);
-            newBoard.set(space, space);
-            bestValue = Integer.min(bestValue, value);
-        }
-        return bestValue;
-    }
-
-    private boolean gameIsOver(ArrayList boardState) {
-        if (ourBoard.gameIsTie(boardState)) {
-            return true;
-        }
-        if (ourBoard.hasAPlayerWon(boardState, aiSymbol)) {
-            return true;
-        }
-        if (ourBoard.hasAPlayerWon(boardState, humanSymbol)) {
-            return true;
-        }
-        return false;
-    }
-
-    private void easyMode(IBoard board) {
-        for (Object space : board.getSpaces()) {
-            if (space instanceof Integer) {
-                board.updateSpace(space.toString(), aiSymbol);
-                break;
-            }
-        }
-    }
-
-    private void hardMode(IBoard board) {
-        ArrayList<Integer> initialSpaces = board.findEmptySpaces(board.getSpaces());
-        Map<Integer, Integer> scoredSpaces = new HashMap<>();
-        int depth = board.getSpaces().size();
-        int pointOfView = 1;
-
-        for (int space : initialSpaces) {
-            ArrayList newBoard = new ArrayList();
-            newBoard.addAll(board.getSpaces());
-            newBoard.set(space, aiSymbol);
-            int value = miniMax(newBoard, depth, pointOfView);
-            scoredSpaces.put(space, value);
-        }
-        int bestMove = findBestMove(scoredSpaces);
-        board.updateSpace(Integer.toString(bestMove), this.aiSymbol);
     }
 
     public void makeMove(IBoard board) {
--- src/test/java/com/EighthLight/app/TestAiDifficulties.java
+++ src/test/java/com/EighthLight/app/TestAiDifficulties.java
@@ -18,7 +18,6 @@ public class TestAiDifficulties {
     @Test
     void markBoardUpdatesTheBoardWithARandomMoveIfGameHasNotEnded() {
         String aiSymbol = "X";
-        String humanSymbol = "O";
         ArrayList boardWithOpenSpace = new ArrayList(Arrays.asList("O", 1));
         ArrayList expectedBoard = new ArrayList(Arrays.asList("O", "X"));
         IBoard board = new Board(3);
@@ -27,6 +26,137 @@ public class TestAiDifficulties {
         easy.markBoard(board);
 
         assertEquals(expectedBoard, board.getSpaces());
+    }
 
+    @Test
+    void aiMakesTheRightMoveWithTwoSpotsLeft() {
+        String aiSymbol = "X";
+        String humanSymbol = "O";
+        ArrayList boardState = new ArrayList(Arrays.asList(
+                "X", 1, 2,
+                "O", "X", "O",
+                "X", "O", "O"));
+        ArrayList expectedBoardState = new ArrayList(Arrays.asList(
+                "X", 1, "X",
+                "O", "X", "O",
+                "X", "O", "O"));
+        Board board = new Board(3);
+
+        HardDifficulty hard = new HardDifficulty(aiSymbol, humanSymbol);
+        setBoardState(board, boardState);
+        hard.markBoard(board);
+
+        assertEquals(expectedBoardState, board.getSpaces());
+    }
+
+    @Test
+    void aiBlocksOpponentWhoIsAboutToWin() {
+        String aiSymbol = "X";
+        String humanSymbol = "O";
+        ArrayList boardState = new ArrayList(Arrays.asList(
+                "O", 1, 2,
+                "O", "X", 5,
+                6, 7, 8));
+
+        ArrayList expectedBoardState = new ArrayList(Arrays.asList(
+                "O", 1, 2,
+                "O", "X", 5,
+                "X", 7, 8));
+        Board board = new Board(3);
+        HardDifficulty hard = new HardDifficulty(aiSymbol, humanSymbol);
+        setBoardState(board, boardState);
+        hard.markBoard(board);
+
+        assertEquals(expectedBoardState, board.getSpaces());
+    }
+
+    @Test
+    void aiBlocksDiagonalHumanWin() {
+        String aiSymbol = "X";
+        String humanSymbol = "O";
+        ArrayList boardState = new ArrayList(Arrays.asList(
+                "X", "X", "O",
+                3, "O", 5,
+                6, 7, 8));
+        ArrayList expectedBoardState = new ArrayList(Arrays.asList(
+                "X", "X", "O",
+                3, "O", 5,
+                "X", 7, 8));
+        Board board = new Board(3);
+        HardDifficulty hard = new HardDifficulty(aiSymbol, humanSymbol);
+        setBoardState(board, boardState);
+        hard.markBoard(board);
+
+        assertEquals(expectedBoardState, board.getSpaces());
+    }
+
+    @Test
+    void aiValuesAWinOverABlock() {
+        String aiSymbol = "X";
+        String humanSymbol = "O";
+        ArrayList boardState = new ArrayList(Arrays.asList(
+                "X", "O", 2,
+                "X", "O", 5,
+                6, 7, 8));
+        ArrayList expectedBoard = new ArrayList(Arrays.asList(
+                "X", "O", 2,
+                "X", "O", 5,
+                "X", 7, 8));
+
+        Board board = new Board(3);
+        HardDifficulty hard = new HardDifficulty(aiSymbol, humanSymbol);
+        setBoardState(board, boardState);
+        hard.markBoard(board);
+
+        assertEquals(expectedBoard, board.getSpaces());
+    }
+
+    @Test
+    void aiBlocksPlayerOnA4x4Board() {
+        String aiSymbol = "X";
+        String humanSymbol = "O";
+        Board board = new Board(4);
+
+        ArrayList boardState = new ArrayList(Arrays.asList(
+                "O", "X", "X", "O",
+                "X", "O", "X", "O",
+                "O", "X", "O", "X",
+                "X", "O", 14, 15));
+        ArrayList expectedBoard = new ArrayList(Arrays.asList(
+                "O", "X", "X", "O",
+                "X", "O", "X", "O",
+                "O", "X", "O", "X",
+                "X", "O", 14, "X"));
+        setBoardState(board, boardState);
+        HardDifficulty hard = new HardDifficulty(aiSymbol, humanSymbol);
+        setBoardState(board, boardState);
+        hard.markBoard(board);
+
+        assertEquals(expectedBoard, board.getSpaces());
+    }
+
+    @Test
+    void aiBlocksPlayerOnA4x4Board9SpotsLeft() {
+        String aiSymbol = "X";
+        String humanSymbol = "O";
+        Board board = new Board(4);
+
+        ArrayList boardState = new ArrayList(Arrays.asList(
+                "O", "X", 2, 3,
+                4, "O", "X", 7,
+                8, 9, "O", "X",
+                12, 13, 14, 15));
+        ArrayList expectedBoard = new ArrayList(Arrays.asList(
+                "O", "X", 2, 3,
+                4, "O", "X", 7,
+                8, 9, "O", "X",
+                12, 13, 14, "X"));
+
+        setBoardState(board, boardState);
+        HardDifficulty hard = new HardDifficulty(aiSymbol, humanSymbol);
+        setBoardState(board, boardState);
+        hard.markBoard(board);
+
+        assertEquals(expectedBoard, board.getSpaces());
     }
 }
